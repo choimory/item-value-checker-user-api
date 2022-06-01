02fe7ce15d8002d73c2047c79d142dd4b50e140f
choimory <choimory@naver.com>
2022-06-01 19:48:33 +0900

    test: 테스트코드 작성

---
+++ src/test/java/com/choimory/itemvaluechecker/api/userapi/config/SpringRestDocsConfig.java
@@ -0,0 +1,17 @@
+package com.choimory.itemvaluechecker.api.userapi.config;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class SpringRestDocsConfig {
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer(){
+        return configurer -> configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+    }
+}
