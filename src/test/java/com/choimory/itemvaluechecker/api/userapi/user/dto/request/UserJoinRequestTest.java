c7a8ed92bd196b5946b4a75c7a7cbf90d2f283eb
choimory <choimory@naver.com>
2022-06-04 14:47:11 +0900

    test: 아이디 검증 테스트코드

--- src/test/java/com/choimory/itemvaluechecker/api/userapi/user/dto/request/UserJoinRequestTest.java
+++ src/test/java/com/choimory/itemvaluechecker/api/userapi/user/dto/request/UserJoinRequestTest.java
@@ -111,41 +111,46 @@ class UserJoinRequestTest {
 
     static Stream<Arguments> requiredArgsValidateMethodSource() {
         return Stream.<Arguments>builder()
-                //성공
+                /*성공*/
                 .add(Arguments.arguments(true, null, "id", "password", "name", null))
-                //아이디 누락
+                /*아이디 누락*/
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "", "password", "name", UserJoinRequest.UserJoinRequestValidate.ID_MISSING))
-                //비밀번호 누락
+                /*비밀번호 누락*/
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "id", "", "name", UserJoinRequest.UserJoinRequestValidate.PASSWORD_MISSING))
-                //이름 누락
+                /*이름 누락*/
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "id", "password", "", UserJoinRequest.UserJoinRequestValidate.NAME_MISSING))
                 .build();
     }
 
     static Stream<Arguments> isIdValidateMethodSource() {
         return Stream.<Arguments>builder()
-                //성공
-                .add(Arguments.arguments(true, null, null, null))
-                //영문, 숫자 외 문자 포함
-                //5글자 미만 혹은 15글자 초과
+                /*성공*/
+                .add(Arguments.arguments(true, null, "choimory99", null))
+                /*영문, 숫자 외 문자 포함*/
+                .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "sdfsdf@#$@#$%%#@", UserJoinRequest.UserJoinRequestValidate.ID_NOT_ENGLISH_AND_NUMBERS_CONTAINED)) // 특문
+                .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "sdweff한글한글한글", UserJoinRequest.UserJoinRequestValidate.ID_NOT_ENGLISH_AND_NUMBERS_CONTAINED)) // 한글
+                .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "s p a c e", UserJoinRequest.UserJoinRequestValidate.ID_NOT_ENGLISH_AND_NUMBERS_CONTAINED)) // 띄어쓰기
+                /*5글자 미만 혹은 15글자 초과*/
+                .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "id", UserJoinRequest.UserJoinRequestValidate.ID_LENGTH_NOT_VALID)) // 5글자 미만
+                .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "idididididididididididd", UserJoinRequest.UserJoinRequestValidate.ID_LENGTH_NOT_VALID)) // 15글자 초과
                 .build();
     }
 
     static Stream<Arguments> isPasswordValidateMethodSource() {
         return Stream.<Arguments>builder()
-                //성공
+                /*성공*/
                 .add(Arguments.arguments(true, null, null, null))
-                //비밀번호 길이 확인
-                //특문 포함 여부
-                //대소문자 포함 여부
+                /*비밀번호 길이 확인*/
+                /*특문 포함 여부*/
+                /*대소문자 포함 여부*/
                 .build();
     }
 
     static Stream<Arguments> isEmailValidateMethodSource() {
         return Stream.<Arguments>builder()
-                //성공
+                /*성공*/
                 .add(Arguments.arguments(true, null, null, null))
-                //이메일 형식 준수 여부
+                /*이메일 형식 준수 여부*/
                 .build();
     }
 }
