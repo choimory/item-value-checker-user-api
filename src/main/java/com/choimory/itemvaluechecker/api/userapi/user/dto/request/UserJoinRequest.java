30a40ae271ad177e09f9b7bc4de4a332f36c56fb
choimory <choimory@naver.com>
2022-06-04 15:37:07 +0900

    style: 이메일 검증 TODO 제거

--- src/main/java/com/choimory/itemvaluechecker/api/userapi/user/dto/request/UserJoinRequest.java
+++ src/main/java/com/choimory/itemvaluechecker/api/userapi/user/dto/request/UserJoinRequest.java
@@ -160,7 +160,6 @@ public class UserJoinRequest {
         return true;
     }
 
-    // TODO
     private boolean isEmailPatternValidate(){
         String pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
         return Pattern.matches(pattern, email);
