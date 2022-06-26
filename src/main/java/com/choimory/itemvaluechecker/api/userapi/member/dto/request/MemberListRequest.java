b2248953b91c9ac9340da010b5950b7f82bb4cb7
choimory <choimory@naver.com>
2022-06-26 11:39:25 +0900

    fix: 일자관련 요청 파라미터 @DateTimeFormat처리, 응답 Json @JsonFormat처리

--- src/main/java/com/choimory/itemvaluechecker/api/userapi/member/dto/request/MemberListRequest.java
+++ src/main/java/com/choimory/itemvaluechecker/api/userapi/member/dto/request/MemberListRequest.java
@@ -4,6 +4,7 @@ import com.choimory.itemvaluechecker.api.userapi.member.code.AuthLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -15,10 +16,16 @@ public class MemberListRequest {
     private final String name;
     private final String email;
     private final AuthLevel authLevel;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime createdFrom;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime createdTo;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime modifiedFrom;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime modifiedTo;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime deletedFrom;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime deletedTo;
 }
