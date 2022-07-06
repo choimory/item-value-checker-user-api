9aff7f9aa5e1b4de2917811a21d662b6e74da4dc
choimory <choimory@naver.com>
2022-07-06 09:48:26 +0900

    test: 회원가입 비밀번호 암호화 테스트코드

--- src/test/java/com/choimory/itemvaluechecker/api/userapi/member/dto/request/MemberJoinRequestTest.java
+++ src/test/java/com/choimory/itemvaluechecker/api/userapi/member/dto/request/MemberJoinRequestTest.java
@@ -1,12 +1,18 @@
 package com.choimory.itemvaluechecker.api.userapi.member.dto.request;
 
 import com.choimory.itemvaluechecker.api.userapi.common.exception.CommonException;
+import com.choimory.itemvaluechecker.api.userapi.member.entity.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.stream.Stream;
 
@@ -162,6 +168,29 @@ class MemberJoinRequestTest {
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "addfgdfg", MemberJoinRequest.MemberJoinRequestValidate.EMAIL_PATTERN_NOT_VALID))
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "addfgdfg@@sdf", MemberJoinRequest.MemberJoinRequestValidate.EMAIL_PATTERN_NOT_VALID))
                 .add(Arguments.arguments(false, HttpStatus.BAD_REQUEST, "addfgdfg@aas!#$sdf.wef", MemberJoinRequest.MemberJoinRequestValidate.EMAIL_PATTERN_NOT_VALID))
+                .build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("toEntityMethodSource")
+    @DisplayName("Request_to_Entity 암호화 테스트")
+    void toEntityParameterizedTest(boolean isSuccess, String password) {
+        /*given*/
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        MemberJoinRequest param = MemberJoinRequest.builder()
+                .password(password)
+                .build();
+
+        /*when*/
+        Member entity = param.toEntity(passwordEncoder);
+
+        /*then*/
+        Assertions.assertThat(isSuccess).isEqualTo(passwordEncoder.matches(password, entity.getPassword()));
+    }
+
+    static Stream<Arguments> toEntityMethodSource(){
+        return Stream.<Arguments>builder()
+                .add(Arguments.of(true, "Asdqwe123!"))
                 .build();
     }
 }
