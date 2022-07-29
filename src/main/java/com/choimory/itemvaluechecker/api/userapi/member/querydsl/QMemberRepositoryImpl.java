e3f522f9c15be826ede21f3d8bb05d4f3ff3b0d7
choimory <choimory@naver.com>
2022-07-29 20:29:04 +0900

    fix: 인피니티 스크롤 페이지 목록 조회 쿼리 작성

--- src/main/java/com/choimory/itemvaluechecker/api/userapi/member/querydsl/QMemberRepositoryImpl.java
+++ src/main/java/com/choimory/itemvaluechecker/api/userapi/member/querydsl/QMemberRepositoryImpl.java
@@ -50,59 +50,19 @@ public class QMemberRepositoryImpl extends Querydsl4RepositorySupport implements
 
     @Override
     public List<MemberDto> findAllNoOffset(int lastId, int size, String identity, String nickname, String email, AuthLevel authLevel, LocalDateTime createdFrom, LocalDateTime createdTo, LocalDateTime modifiedFrom, LocalDateTime modifiedTo, LocalDateTime deletedFrom, LocalDateTime deletedTo) {
-        //임시 - 일단 전체 변환하는 방법
-        /*select(Projections.fields(MemberDto.class, member))
-                .from(member)
-                .innerJoin(member.memberAuthority, memberAuthority)
-                .innerJoin(member.memberSocials, memberSocial).fetchJoin()
-                .where(gtId(lastId),
-                        eqIdentity(identity),
-                        containsNickname(nickname),
-                        containsEmail(email),
-                        eqAuthLevel(authLevel),
-                        betweenCreatedAt(createdFrom, createdTo),
-                        betweenModifiedAt(modifiedFrom, modifiedTo),
-                        betweenDeletedAt(deletedFrom, deletedTo))
-                .limit(size)
-                .fetch();*/
-
-        //삽질 1. memberDto의 1:1 객체인 memberAuthority까지만 dto 안에 넣고 List<MemberSocialDto>은 DTO 바깥으로 따로 빼서 일단 튜플(dto + list)로 반환하는데까지 확인
-        /*query.select(Projections.fields(MemberDto.class,
-                member.identity,
-                Projections.fields(MemberDto.MemberAuthorityDto.class,
-                        memberAuthority.authLevel).as("memberAuthority")
-                ),
-                Projections.list(
-                        memberSocial.socialType,
-                        memberSocial.socialId)
-        )
-                .from(member)
-                .innerJoin(member.memberAuthority, memberAuthority)
-                .innerJoin(member.memberSocials, memberSocial)
-                .where(gtId(lastId),
-                        eqIdentity(identity),
-                        containsNickname(nickname),
-                        containsEmail(email),
-                        eqAuthLevel(authLevel),
-                        betweenCreatedAt(createdFrom, createdTo),
-                        betweenModifiedAt(modifiedFrom, modifiedTo),
-                        betweenDeletedAt(deletedFrom, deletedTo))
-                .limit(size)
-                .fetch();*/
-
-        /*정답 찾음.*/
-        // 1. memberDto의 필드들과 1:1 객체들은 projections.fields()로 MeberDto로 바로 변환, List<MemberSocialDto>는 Projections.list()로 dto 바깥으로 빼서 별도의 튜플 객체를 만든 뒤 (삽질1)
-        // 2. querydsl의 .transform(GroupBy.groupBy().list(GroupBy.list()).as())로 result aggregation하여 1:N 객체도 Member dto에 최종 변환하여 반환
-        // 3. 페이징 처리는 힘듦
-
-
-        return query.select(Projections.fields(MemberDto.class,
-                member.id, member.identity, member.nickname,
-                Projections.fields(MemberDto.MemberAuthorityDto.class,
-                        memberAuthority.authLevel).as("memberAuthority")
-                ),
-                Projections.list(
-                        memberSocial.socialType, memberSocial.socialId)
+        return query.select(
+                    Projections.fields(MemberDto.class,
+                            member.id,
+                            member.identity,
+                            member.nickname,
+                            Projections.fields(MemberDto.MemberAuthorityDto.class,
+                                    memberAuthority.authLevel
+                            ).as("memberAuthority")
+                    ),
+                    Projections.list(
+                            memberSocial.socialType,
+                            memberSocial.socialId
+                    )
                 )
                 .from(member)
                 .where(gtId(lastId),
@@ -114,13 +74,10 @@ public class QMemberRepositoryImpl extends Querydsl4RepositorySupport implements
                         betweenModifiedAt(modifiedFrom, modifiedTo),
                         betweenDeletedAt(deletedFrom, deletedTo))
                 .innerJoin(member.memberAuthority, memberAuthority)
-                .leftJoin(member.memberSocials, memberSocial).fetchJoin()
+                .leftJoin(member.memberSocials, memberSocial)
                 .transform(GroupBy.groupBy(member.identity)
                         .list(Projections.fields(MemberDto.class, member.id, member.identity, member.nickname,
                                 Projections.fields(MemberDto.MemberAuthorityDto.class, memberAuthority.authLevel).as("memberAuthority"),
                                 GroupBy.list(Projections.fields(MemberDto.MemberSocialDto.class, memberSocial.socialType, memberSocial.socialId)).as("memberSocials"))));
-
-
-
     }
 }
